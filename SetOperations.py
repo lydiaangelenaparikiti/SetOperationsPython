db4763d6469c448ce6e2d0c7d11c9240dc10e6b7
Sanju <lydiaangelenaparikiti@gmail.com>
2025-09-07 22:46:47 +0530

    Simplify input for Set Operations program

--- SetOperations.py
+++ SetOperations.py
@@ -1,17 +1,17 @@
 def union_sets(set1, set2):
-    """Return the union of two sets"""
+    "Return the union of two sets"
     return set1.union(set2)
 
 def intersection_sets(set1, set2):
-    """Return the intersection of two sets"""
+    "Return the intersection of two sets"
     return set1.intersection(set2)
 
 def difference_sets(set1, set2):
-    """Return the difference of set1 - set2"""
+    "Return the difference of set1 - set2"
     return set1.difference(set2)
 
 def is_subset(set1, set2):
-    """Check if set1 is a subset of set2"""
+    "Check if set1 is a subset of set2"
     return set1.issubset(set2)
 
 # Function to read a set from user input
@@ -25,10 +25,10 @@ setB = read_set("Set B")
 print("\nSet A:", setA)
 print("Set B:", setB)
 
-print("1️⃣ Union:", union_sets(setA, setB))
-print("2️⃣ Intersection:", intersection_sets(setA, setB))
-print("3️⃣ Difference (A - B):", difference_sets(setA, setB))
-print("4️⃣ Difference (B - A):", difference_sets(setB, setA))
-print("5️⃣ Is A a subset of B?:", is_subset(setA, setB))
-print("6️⃣ Is B a subset of A?:", is_subset(setB, setA))
+print("Union:", union_sets(setA, setB))
+print("Intersection:", intersection_sets(setA, setB))
+print("Difference (A - B):", difference_sets(setA, setB))
+print("Difference (B - A):", difference_sets(setB, setA))
+print("Is A a subset of B?:", is_subset(setA, setB))
+print("Is B a subset of A?:", is_subset(setB, setA))
 
